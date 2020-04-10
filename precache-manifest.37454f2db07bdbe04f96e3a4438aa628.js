8ca7c1f858012793117ae1dafdd9df76c5877dd4
Caio <caiookb@gmail.com>
2020-04-09 23:54:32 -0300

    Updates

---
+++ precache-manifest.37454f2db07bdbe04f96e3a4438aa628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d798f65c49cb9cc3f1b6d03e017eae2",
+    "url": "/moodar-stress/index.html"
+  },
+  {
+    "revision": "8793f89d2c038733eec8",
+    "url": "/moodar-stress/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8cfe7174046e3a4c7279",
+    "url": "/moodar-stress/static/css/main.b5aa385d.chunk.css"
+  },
+  {
+    "revision": "8793f89d2c038733eec8",
+    "url": "/moodar-stress/static/js/2.2510855b.chunk.js"
+  },
+  {
+    "revision": "8cfe7174046e3a4c7279",
+    "url": "/moodar-stress/static/js/main.8dd824d8.chunk.js"
+  },
+  {
+    "revision": "6cff551dbf20f90f26e0",
+    "url": "/moodar-stress/static/js/runtime-main.d132401d.js"
+  }
+]);
